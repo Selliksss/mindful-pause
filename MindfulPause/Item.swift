a9b7bfbf280324ee702d7c18a6d8540ebcb1dfc8
Selliksss <277231264+Selliksss@users.noreply.github.com>
2026-05-07 02:56:37 +0300

    Initial Commit

---
+++ MindfulPause/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MindfulPause
+//
+//  Created by Lev Tarasov on 07.05.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
